f78c1f57dbf3c500ad800ba444c97de272d1c94f
Rey <matsunagar97@gmail.com>
2018-12-21 13:09:22 +0900

    fixed navbar

--- src/components/UGCNavbar.js
+++ src/components/UGCNavbar.js
@@ -1,41 +1,92 @@
 import React, { Component } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 import '../App.css';
 import Logo from '../ugc_logo.png';
 
 class UGCNavbar extends Component {
-    render(){
-      return(
-        <div className="topnav" id="myTopnav">
-          <img src={Logo} alt="" className="logo"/>
-          <a href="/" class="active">Home</a>
-          <div className="dropdown">
-            <button className="dropbtn">About 
-              <i className="fa fa-caret-down"/>
-            </button>
-            <div className="dropdown-content">
-              <a href="/about">Overview</a>
-              <a href="/boards">Executive Board</a>
-              <a href="/achievements">Achievements</a>
-              <a href="#">Documents</a>
-            </div>
-          </div> 
-          <a href = '/organizations'>Organizations</a>
-          <a href="/achievementsandboards">Achievements & Boards</a>
-          <a href = '/blog'>Blog</a>
-          <a href = '/calendar'>Calendar</a>
-          <a href = '#'>Contact</a>
-          <a href="javascript:void(0);" className="icon" onClick={myFunction => {
-                      var x = document.getElementById("myTopnav");
-                      if (x.className === "topnav") {
-                          x.className += " responsive";
-                      } else {
-                          x.className = "topnav";
-                      }
-                    }
-          }>&#9776;</a>
-        </div>
-      );
-    }
-  }
+  constructor(props) {
+    super(props);
 
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar className = "navBar" color="light" light expand="md">
+          <NavbarBrand href='/'>
+            <img src={Logo} alt="" className="logo"/> 
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                    About
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <LinkContainer to="/about">
+                    <DropdownItem>
+                      Overview
+                    </DropdownItem>
+                  </LinkContainer>
+                  <LinkContainer to="/#">
+                    <DropdownItem>
+                      Executive Board
+                    </DropdownItem>
+                  </LinkContainer>
+                  <LinkContainer to="/#">
+                    <DropdownItem>
+                      Achievements
+                    </DropdownItem>
+                  </LinkContainer>
+                  <LinkContainer to="/#">
+                    <DropdownItem>
+                      Documents
+                    </DropdownItem>
+                  </LinkContainer>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink href = '/organizations'>Organizations</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href = '/blog'>Blog</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href = '/calendar'>Calendar</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href = '/#'>Contact</NavLink>
+              </NavItem>
+              
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
   export default UGCNavbar;
