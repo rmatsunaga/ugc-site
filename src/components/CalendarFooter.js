013ecc6a22a236f1ad4f3669a378ca5334745b59
Rey <matsunagar97@gmail.com>
2018-12-26 17:48:25 +0900

    fixed footers

--- src/components/CalendarFooter.js
+++ src/components/CalendarFooter.js
@@ -3,9 +3,9 @@ import React from "react";
 class CalendarFooter extends React.Component {
   render() {
     return (
-      <div className = "flex-container-presidential">
+      <div className = "flex-container">
         <hr />
-        <div className = "flex-content-presidential">
+        <div className = "flex-content">
         &copy; {new Date().getFullYear()} Copyright:{" "}
             Rey Matsunaga, CJay Zhijie Hon, Ashley Djohan
         </div>
