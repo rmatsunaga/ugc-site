fabd598ad23d89916cac42877353c150f92349d0
Rey <matsunagar97@gmail.com>
2019-01-02 23:32:01 +0900

    refactor

--- src/components/AboutOverviewFooter.js
+++ src/components/AboutOverviewFooter.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-class AboutOverviewFooter extends React.Component {
-  render() {
-    return (
-      <div className = "flex-container">
-        <hr />
-        <div className = "flex-content">
-        &copy; {new Date().getFullYear()} Copyright:{" "}
-            Rey Matsunaga, CJay Zhijie Hon, Ashley Djohan
-        </div>
+const AboutOverviewFooter = () => {
+  return (
+    <div className = "flex-container">
+      <hr />
+      <div className = "flex-content">
+      &copy; {new Date().getFullYear()} Copyright:{" "}
+          Rey Matsunaga, CJay Zhijie Hon, Ashley Djohan
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default AboutOverviewFooter;
